68586a1d20a0453ecdada71db19ecc100f517adc
alfre <alfredo.medina@uc.cl>
2023-09-26 00:29:00 -0300

    add: shadcn/ui

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function App(): JSX.Element {
-  return <div className="text-3xl font-bold underline">Hello World</div>;
+  return (
+    <>
+      <div className="text-3xl font-bold underline">Hello World</div>
+      <Button>Click me</Button>
+    </>
+  );
 }
