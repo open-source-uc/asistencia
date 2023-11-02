f158b0def8f620bd1db37e09511432edcd4605fb
alfre <alfredo.medina@uc.cl>
2023-11-02 18:40:31 -0300

    hotfix

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -1,7 +1,7 @@
+/* eslint-disable react-refresh/only-export-components */
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-// import spinner
 import LoadingSpinner from "../loading-spinner";
 
 import { cn } from "@/lib/utils";
@@ -69,4 +69,4 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 );
 Button.displayName = "Button";
 
-export { Button };
+export { Button, buttonVariants };
