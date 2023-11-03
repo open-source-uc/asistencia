2bee5a6b5cfea338d976bce13b1de7cbecac1d06
alfre <alfredo.medina@uc.cl>
2023-11-02 21:48:44 -0300

    add: sha512 and clientHash functions

---
+++ apps/web/src/lib/hashFunctions.ts
@@ -0,0 +1,16 @@
+const sha512 = async (str: string): Promise<string> => {
+  return crypto.subtle
+    .digest("SHA-512", new TextEncoder().encode(str))
+    .then((buf) => {
+      return Array.prototype.map
+        .call(new Uint8Array(buf), (x) => ("00" + x.toString(16)).slice(-2))
+        .join("");
+    });
+};
+
+const clientHash = async (code: string, courseId: string): Promise<string> => {
+  const hash = await sha512(sha512(code + courseId) + courseId);
+  return hash;
+};
+
+export { sha512, clientHash };
