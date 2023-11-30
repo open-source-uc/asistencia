c4f862870a4b8a202eaddbc183f6ee06e5aaada8
Benjamín Vicente <benjavicente@uc.cl>
2023-11-30 20:09:08 -0300

    chore(sheets): fix for new backend

--- apps/fn-google-sheets/src/functions.ts
+++ apps/fn-google-sheets/src/functions.ts
@@ -19,9 +19,28 @@ function asRow(row: AnySheetsInput, errorMsg = "Debe ser una fila") {
   throw new Error(errorMsg);
 }
 
-const BASE_URL = "http://attendance.cparedesr.com/api/";
+const BASE_URL = "https://attendance.cparedesr.com";
+const BASE_API_URL = BASE_URL + "/api/v1/";
 
 const ASSISTANCE_URL = "sheets/check_assistance";
+
+/**
+ * [DEPRECADO: Usar ATTENDANCE]
+ * Ve la asistencia de los participantes en las actividades
+ * @deprecated
+ * @customfunction
+ */
+function ASSISTANCE(
+  courseId: string,
+  participants: AnySheetsInput,
+  activities: AnySheetsInput,
+  _variableInput: any = undefined,
+  email: string,
+  token: string
+) {
+  ATTENDANCE(email, token, courseId, participants, activities, _variableInput);
+}
+
 /**
  * Ve la asistencia de los participantes en las actividades
  * @param courseId Id del curso
@@ -30,27 +49,39 @@ const ASSISTANCE_URL = "sheets/check_assistance";
  * @param _variableInput Variable cualquiera de entrada, para forzar una actualización
  * @customfunction
  */
-function ASSISTANCE(courseId: string, participants: AnySheetsInput, activities: AnySheetsInput, _variableInput?: any) {
+function ATTENDANCE(
+  email: string,
+  token: string,
+  courseId: string,
+  participants: AnySheetsInput,
+  activities: AnySheetsInput,
+  _variableInput?: any
+) {
   const flattenParticipants = asColumn(participants).map((e) => e?.toString() ?? "");
   const flattenActivities = asRow(activities).map((e) => e?.toString() ?? "");
 
   try {
-    const response = UrlFetchApp.fetch(BASE_URL + ASSISTANCE_URL, {
+    const response = UrlFetchApp.fetch(`${BASE_API_URL}courses/${courseId}/spreadsheets`, {
       method: "post",
       contentType: "application/json",
+      headers: {
+        "X-User-Email": email,
+        "X-User-Token": token,
+        Accept: "application/json",
+      },
       payload: JSON.stringify({
-        activities_slugs: flattenActivities,
-        students_ids: flattenParticipants,
-        course_id: courseId,
+        activity_slugs: flattenActivities,
+        student_codes: flattenParticipants,
       }),
     });
+
     const body = response.getContentText();
     const data: Record<string, Record<string, any>> = JSON.parse(body);
+
     return flattenParticipants.map((student) =>
       flattenActivities.map((activity) => {
         if (!(student in data)) return "";
-        if (!(activity in data[student])) return "";
-        return true;
+        return data[student].includes(activity) ? true : "";
       })
     );
   } catch (error) {
@@ -64,8 +95,8 @@ function ASSISTANCE(courseId: string, participants: AnySheetsInput, activities: 
  */
 function HEALTH_ASSISTANCE_BACKEND() {
   try {
-    const response = UrlFetchApp.fetch(BASE_URL + "health_check");
-    return response.getResponseCode() === 200 ? "OK" : "ERROR";
+    const response = UrlFetchApp.fetch(BASE_URL);
+    return response.getResponseCode() === 200;
   } catch (error) {
     return error;
   }
