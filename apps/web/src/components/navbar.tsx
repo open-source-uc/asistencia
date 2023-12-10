c0f735ab94ca163a6dbc49ca95dbc29bf0ac28ec
alfre <alfredo.medina@uc.cl>
2023-12-10 13:23:38 -0300

    fix: navbar events for web responsive

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -1,57 +1,59 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { useOnClickOutside } from "@/hooks/useOnClickOutside";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import { useNavigate } from "react-router-dom";
 import { MenuIcon } from "lucide-react";
 import { BARTABS } from "@/lib/constants/barTabs";
 
-
 export function NavBar({ className }: { className?: string }) {
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
+  const navRef = useRef(null);
+  useOnClickOutside(navRef, () => setIsOpen(false));
   return (
-    <div className={className}>
+    <div className={className} ref={navRef}>
       <div
         className={cn(
           isOpen ? "h-56" : "h-16",
           "fixed bg-primary text-primary-foreground w-full flex flex-col justify-start items-start py-3 shadow-md z-10 overflow-hidden"
         )}
       >
-          <div className="flex flex-row justify-start items-center w-full">
+        <div className="flex flex-row justify-start items-center w-full">
+          <Button
+            variant={"ghost"}
+            className="rounded-full ml-4 p-2"
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            <MenuIcon />
+          </Button>
+          <Button
+            className="px-4 text-2xl font-semibold tracking-tight px-6"
+            variant={"noshadow"}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            AttendanceUC
+          </Button>
+        </div>
+        <div className="space-y-2 pt-3 w-full">
+          {BARTABS.map((link, i) => (
             <Button
-              variant={"ghost"}
-              className="rounded-full ml-4 p-2"
+              key={i}
+              variant="ghost"
+              className="w-full justify-start py-6 px-6"
               onClick={() => {
-                setIsOpen(!isOpen);
+                navigate(link.path);
               }}
             >
-              <MenuIcon />
+              <link.icon />
+              {isOpen && <span className="ml-6">{link.name}</span>}
             </Button>
-            <Button
-              className="px-4 text-2xl font-semibold tracking-tight px-6"
-              variant={"noshadow"}
-              onClick={() => {
-                navigate("/");
-              }}
-            >
-              AttendanceUC
-            </Button>
-          </div>
-          <div className="space-y-2 pt-3 w-full">
-            {BARTABS.map((link, i) => (
-              <Button
-                key={i}
-                variant="ghost"
-                className="w-full justify-start py-6 px-6"
-                onClick={() => {
-                  navigate(link.path);
-                }}
-              >
-                <link.icon />
-                {isOpen && <span className="ml-6">{link.name}</span>}
-              </Button>
-            ))}
-          </div>
+          ))}
+        </div>
       </div>
     </div>
   );
